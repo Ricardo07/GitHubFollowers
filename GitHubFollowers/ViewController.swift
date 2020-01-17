a88effbfde0436a0772143fd8c336c1825560f49
Ricardo Batista <ricardo.batista07@gmail.com>
2020-01-17 15:24:00 -0400

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Ricardo Batista on 1/17/20.
+//  Copyright © 2020 Ricardo Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
