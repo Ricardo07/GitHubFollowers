97d882a2296b80aa74d534c97d10ab902582d072
Ricardo Batista <ricardo.batista07@gmail.com>
2020-01-18 13:39:35 -0400

    Adding custom reusable button and textfield

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Ricardo Batista on 1/18/20.
+//  Copyright © 2020 Ricardo Batista. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // this will handle initialization via storyboard
+    // but we are not using it on this project
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
