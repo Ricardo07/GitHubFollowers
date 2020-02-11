efd4404c27c4d5f63dabc3e25cb857abdf640401
Ricardo Batista <ricardo.batista07@gmail.com>
2020-02-10 23:55:23 -0400

    Implementing network call to download images for follewer collection view and implementing the cache

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(named: "avatar-placeholder")! // its force unwrapped beacuse we now that the image is in the bundle
 
     override init(frame: CGRect) {
@@ -27,5 +28,36 @@ class GFAvatarImageView: UIImageView {
         clipsToBounds = true
         image = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            // none of the fallowing error are handled because if something goes wrong we want the placeholder image to take place
+            guard let self = self else { return }
+            
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            guard let image = UIImage(data: data) else { return }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
     }
 }
