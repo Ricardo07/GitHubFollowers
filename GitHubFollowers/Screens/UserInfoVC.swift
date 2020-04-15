f61fbc779850bb5a5be089926e4d4bcea6bb77e3
Ricardo Batista <ricardo.batista07@gmail.com>
2020-04-15 17:26:06 -0400

    Implementing modal presentation of UserInfoVC

---
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by Ricardo Batista on 4/15/20.
+//  Copyright © 2020 Ricardo Batista. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
