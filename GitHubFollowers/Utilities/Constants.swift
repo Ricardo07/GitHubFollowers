4b870c9a53821f2da9c027e0bc90ad69ca9bb296
Ricardo Batista <ricardo.batista07@gmail.com>
2020-04-15 20:11:12 -0400

    Implementing UserInfoHeadingVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Ricardo Batista on 4/15/20.
+//  Copyright © 2020 Ricardo Batista. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
